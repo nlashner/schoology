71e522ca43718571fb114e7eb33414672ddb0507
Nora Lashner <nlashner@gmail.com>
2020-08-20 12:10:07 -0400

    initial commit

---
+++ schoology/server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const PORT = 3000
+const app = express()
+const states = require('./states')
+app.use(express.static('public'))
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+
+app.get('/api/states', function (req, res) {
+  res.json(states)
+})
+
+app.get('/search', function (req, res) {
+  let searchTerm = req.query.value
+  let results = []
+  for(let i = 0; i < states.length; i++){
+    let state = states[i].toLowerCase()
+    if(state.startsWith(searchTerm.toLowerCase())){
+      results.push(state)
+    }
+  }
+  res.json(results)
+})
